cf4e7c0653bb89466e49f9713e468122f32456ac
up210063 <up210063@alumnos.upa.edu.mx>
2022-10-19 19:58:01 -0500

    Unit 1

---
+++ U1/01_mundo.cpp
@@ -0,0 +1,21 @@
+ /*
+ iostream = input /output 
+ stream(flujo)
+ */
+
+//------------PREPROCESSOR DIRECTIVES--------------------
+//input/output library between the screen & keyboard
+
+#include <iostream> 
+
+//---------------MAIN FUNCTION--------------- 
+ int main (){
+
+    //print on screen the text
+    std::cout << "Hello World!! This is my first program in cpp(c++)" << std::endl;
+
+
+
+    //Return 0 means that the programm ends succesfully
+    return 0;
+ }
